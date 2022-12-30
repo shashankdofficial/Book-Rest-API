4554fd367f943c3f1e65504710d8b9eee682f6b5
shashankdofficial <allyouget14@gmail.com>
2022-12-30 18:40:48 +0530

    Dynamic File Upload

--- src/main/java/com/api/book/helper/FileUploadHelper.java
+++ src/main/java/com/api/book/helper/FileUploadHelper.java
@@ -1,17 +1,25 @@
 package com.api.book.helper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-	public final String UPLOAD_DIR="E:\\Shashank\\New folder\\Learning Spring\\bootrestbook\\src\\main\\resources\\static\\image";
+//	public final String UPLOAD_DIR="E:\\Shashank\\New folder\\Learning Spring\\bootrestbook\\src\\main\\resources\\static\\image";
+	
+	public final String UPLOAD_DIR = new ClassPathResource("static/image").getFile().getAbsolutePath();
+	
+	public FileUploadHelper() throws IOException {
+		
+	}
 	
 	public boolean uploadFile(MultipartFile file) {
 		boolean f = false;
